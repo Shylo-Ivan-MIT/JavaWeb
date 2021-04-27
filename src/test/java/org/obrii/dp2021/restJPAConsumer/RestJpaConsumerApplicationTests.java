aa05ae09a1946aba2203035ad893a60bc2a6a14f
Roman Mykolaichuk <mykroman@ukr.net>
2021-04-27 19:09:01 +0300

    first

---
+++ src/test/java/org/obrii/dp2021/restJPAConsumer/RestJpaConsumerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.obrii.dp2021.restJPAConsumer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestJpaConsumerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
